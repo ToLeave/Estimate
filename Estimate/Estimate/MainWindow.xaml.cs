37faa9d2782fb3273a72bafa530e34618185a366
ToLeave <hy888716@gmail.com>
2016-02-25 10:16:07 +0800

    2016.2.25

--- Estimate/Estimate/MainWindow.xaml.cs
+++ Estimate/Estimate/MainWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Xceed.Wpf.AvalonDock.Layout;
 
 namespace Estimate
 {
@@ -22,6 +23,71 @@ namespace Estimate
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        public static T FindChild<T>(DependencyObject parent, string childname)//查找控件
+            where T : DependencyObject
+        {
+            if (parent == null) return null;
+            T foundchild = null;
+            int childrencount = VisualTreeHelper.GetChildrenCount(parent);
+            for (int i = 0; i < childrencount; i++)
+            {
+                var child = VisualTreeHelper.GetChild(parent, i);
+                //如果子控件不是需查找的控件类型
+                T childType = child as T;
+                if (childType == null)
+                {
+                    //下一级递归查找
+                    foundchild = FindChild<T>(child, childname);
+                    //找到控件中断递归
+                    if (foundchild != null) break;
+                }
+                else if (!string.IsNullOrEmpty(childname))
+                {
+                    var frameworkElement = child as FrameworkElement;
+                    //如果控件名称符合参数条件
+                    if (frameworkElement != null && frameworkElement.Name == childname)
+                    {
+                        foundchild = (T)child;
+                        break;
+                    }
+                }
+                else
+                {
+                    //查找到控件
+                    foundchild = (T)child;
+                    break;
+                }
+            }
+            return foundchild;
+        }
+
+
+        private void Weigh_Click(object sender, RoutedEventArgs e)
+        {
+        //    Weigh weigh = MainWindow.FindChild<Weigh>(Application.Current.MainWindow, "Weigh");
+        //    if (weigh != null)
+        //    {
+        //        foreach (var t in mainpenal.Children)
+        //        {
+        //            if (t.Title == "过磅单")
+        //            {
+        //                t.IsActive = true;
+        //                break;
+        //            }
+        //        }
+        //    }
+        //    else
+        //    {
+        //        Weigh newweige = new Weigh();
+        //        LayoutDocument newreport = new LayoutDocument();
+        //        newreport.Title = "过磅单";
+        //        newreport.IsActive = true;
+        //        newreport.Content = newweige;
+        //        newweige.Name = "Weigh";
+        //        mainpenal.Children.Add(newreport);
+        //    }
         }
     }
 }
